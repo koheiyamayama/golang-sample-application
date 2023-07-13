1920b9322b7363396a126a1b940a62f6ac5cb784
koheiyamayama <kohei.y.23@gmail.com>
2023-07-13 21:20:49 +0900

    edit Dockerfile.lab for linux os

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -1,3 +1,3 @@
-echo $KS_LABORATORY_BACKEND_WRITE_PACKAGES | docker login ghcr.io -u USERNAME --password-stdin
+echo $KS_LABORATORY_BACKEND_WRITE_PACKAGES | docker login ghcr.io -u koheiyamayama --password-stdin
 docker build -f Dockerfile.lab -t "ghcr.io/koheiyamayama/ks-laboratory-backend:$(git rev-parse HEAD)" .
 docker push "ghcr.io/koheiyamayama/ks-laboratory-backend:$(git rev-parse HEAD)"
